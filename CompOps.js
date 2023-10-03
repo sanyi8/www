6339b7c24d2fe0b4e04a911a3b1fc46994fa7955
Sandor Kardos <sanyitelo@gmail.com>
2023-10-03 11:56:40 +0100

    Practice Comparison Operators

---
+++ CompOps.js
@@ -0,0 +1,17 @@
+var andAge = 25;
+
+
+// if statement condition AND
+if (andAge >= 18 && andAge <=30){
+  document.write("You can come, You cool dude");
+} else {
+  document.write("You aint coming");
+}
+
+var orAge = 25;
+// if statement condition OR
+if (orAge < 18 || orAge > 30 || orAge === 25){
+  document.write("You aint coming");
+} else {
+  document.write("Common Dude!");
+}
