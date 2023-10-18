060fd78ebc82f13e945b0b83a4860ccdf1b2b06b
Sandor Kardos <sanyitelo@gmail.com>
2023-10-18 17:18:11 +0100

    sum two numbers

---
+++ practiceFunctions.js
@@ -0,0 +1,9 @@
+//Sum two numbers
+// The function is declared with two parameters a and b.
+function add(a, b) {
+console.log(a + b);
+}
+// calling function with two arguments
+add(2,5);
+// calling "add" function multiple times with different arguments
+add(3,9);
