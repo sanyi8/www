e50851ffb338326e93c2f8525acc4e72b6f2e319
Sandor Kardos <sanyitelo@gmail.com>
2023-10-18 17:26:03 +0100

    add two numbers using "result" to call add()

--- practiceFunctions.js
+++ practiceFunctions.js
@@ -1,4 +1,4 @@
-//Sum two numbers
+/* //Sum two numbers
 // The function is declared with two parameters a and b.
 function add(a, b) {
 console.log(a + b);
@@ -7,3 +7,19 @@ console.log(a + b);
 add(2,5);
 // calling "add" function multiple times with different arguments
 add(3,9);
+ */
+
+//Sum of Two Numbers
+// declare function with 2 variables
+function add(a, b) {
+  return a + b;
+}
+// get 2 numbers from user
+let number1 = parseFloat(prompt("Enter first number: "));
+let number2 = parseFloat(prompt("Enter second number: "));
+
+//to get "result"s value we calling function and using given parameters: number1 and ...2
+let result = add(number1, number2);
+
+// we write out (alert) the "result" which calling the "add" function passing the arguments as a and b
+alert("The sum is: " + result);
