60312d222f4dddfcd446c5258b33098ef262070d
sanyi8 <sanyitelo@gmail.com>
2024-02-16 16:42:38 +0000

    challenge1 - numberic array

---
+++ WebTech-BootstrapWebsite/remake/challenge1.php
@@ -0,0 +1,12 @@
+
+<?php
+//Numeric Array
+# Create and initialise an array variable.
+echo"<h1>Seasons of the Year</h1>";
+$seasons = array( 'Winter' , 'Spring' , 'Summer' , 'Autumn' ) ;
+
+# Display the value in all elements as a bulleted list.
+foreach( $seasons as $value ) { echo "
+$value 
+ " ; } 
+?>
